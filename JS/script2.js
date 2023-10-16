a90491dc2a7bdc182720d8ce5256c78f086514a0
AndrewShanks02 <as2202@hw.ac.uk>
2023-10-16 22:18:47 +0100

    added binary to int conversion

--- JS/script2.js
+++ JS/script2.js
@@ -6,7 +6,16 @@ function fetchInput()
 {
     return $("#inputField").val();
 }
+function binToInt(s)
+{
+    let n = 0;
+    for (let i = 1; i <= s.length; i++)
+    {
+        n += s[s.length-i] * Math.pow(2,i-1);
+    }
+    return n;
+}
 
 $("#btn").click((e) => {
-    output(fetchInput());
+    output(castBinToInt(fetchInput()));
 });
